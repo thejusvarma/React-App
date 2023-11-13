908166cb1ce1a2c91d9996cb4c5b57b8e9c7c92a
Thejus Varma <thejusvarma11@gmail.com>
2023-11-13 21:48:41 +0530

    Alert messages added

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import Alert from './components/Alert';
 import { useState } from 'react';
 
 function App() {
   const[mode, setMode] = useState("white")
 
+  const[alert,setAlert] = useState(null);
 
   const[myStyle, setStyle] = useState({ color: "black",
   backgroundColor: "white"})
 
   const[toggle,setToggle] = useState({text : "Enable Dark Mode", color : "dark"})
+
+  const showAlert=(message,type)=>
+  {
+    setAlert({msg:message, type:type})
+  }
 
   const toggleMode=()=>
   {
@@ -23,6 +30,7 @@ function App() {
         setStyle({ color: "white",
         backgroundColor: "#1a1a1a",
         borderColor: "grey"})
+        showAlert("Dark Mode has been Enabled","warning");
       }
       else
       {
@@ -31,12 +39,14 @@ function App() {
         document.body.style.backgroundColor = "white";
         setStyle({ color: "black",
         backgroundColor: "white"})
+        showAlert("Light Mode has been Enabled","primary");
       }
   }
 
   return (
     <>
     <Navbar title = "SIPO" mode={mode} toggleMode={toggleMode} toggle={toggle}/>
+    <Alert alert = {alert}/>
     <TextForm myStyle={myStyle} toggle={toggle}/> 
     <About myStyle={myStyle} />
     </>
